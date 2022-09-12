0f56cb5345aa8e1f0ddb93f9f9bbc39127413c4a
Toni Panacek <toni.panacek@gmail.com>
2022-09-12 15:02:22 +0200

    Update landing page with navbar

--- src/assets/scripts/nav-menu.js
+++ src/assets/scripts/nav-menu.js
@@ -1,4 +1,4 @@
-const handleNavMenu = () => {
+const handleHamburgerMenu = () => {
   const hamburger = document.querySelector('.menu-btn');
   const menu = document.querySelector('.menu');
 
@@ -9,4 +9,21 @@ const handleNavMenu = () => {
   }
 }
 
-export { handleNavMenu }
+const updateLandingNavbar = () => {
+  const logo = document.querySelector('.home-btn h1');
+  const nav = document.querySelector('.landing-nav');
+
+  if (nav) {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY >= window.innerHeight - 100) {
+        nav.classList.add('bg-white');
+        logo.classList.add('smaller-logo');
+      } else {
+        nav.classList.remove('bg-white');
+        logo.classList.remove('smaller-logo');
+      }
+    })
+  }
+}
+
+export { handleHamburgerMenu, updateLandingNavbar }
